21782e8894828c222050e6ae4478a9a0ac78b1b9
Keshav Singh <keshavsingh4522@gmail.com>
2021-07-07 19:57:09 +0530

    reflection and Attribute

---
+++ Attribute_And_Reflection/Attribute/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Attribute
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var result = Calculator.Add(12,23);
+            Console.WriteLine("12 + 13 = {0}", result);
+
+            result = Calculator.Add(new List<int>() { 10,20,30});
+            Console.WriteLine("10 + 20 + 30 = {0}", result);
+        }
+    }
+    class Calculator
+    {
+        // generate error if you dont to like the old method
+        // [Obsolete(" Use public static int Add(List<int> arr) Method",true)]
+
+        // warning
+        // [Obsolete]
+
+        // warning with message
+        [Obsolete(" Use public static int Add(List<int> arr) Method")]
+        public static int Add(int x, int y)
+        {
+            return x + y;
+        }
+
+        public static int Add(List<int> arr)
+        {
+            int sum = 0;
+            arr.ForEach(x => sum += x);
+            return sum;
+        }
+    }
+}
---
+++ Attribute_And_Reflection/Reflection_1/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Reflection;
+
+namespace Reflection_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Type
+            // same behaviour
+
+            //Type T = Type.GetType("Reflection_1.Customer");
+
+            //Type T = typeof(Customer);
+
+            Customer c1 = new Customer();
+            Type T = c1.GetType();
+
+            #endregion
+
+
+            Console.WriteLine("Full name : {0}", T.FullName);
+            Console.WriteLine("Just name : {0}", T.Name);
+            Console.WriteLine("namespace name : {0}", T.Namespace);
+
+            Console.WriteLine("\n ------------------ Properties in Customers ------------------");
+            PropertyInfo[] properties = T.GetProperties();
+            foreach(PropertyInfo property in properties)
+            {
+                Console.WriteLine(property.Name+" : "+property.PropertyType.Name);
+            }
+
+            Console.WriteLine("\n ------------------ Methods in Customers ------------------");
+            MethodInfo[] methods = T.GetMethods();
+            foreach(MethodInfo method in methods)
+            {
+                Console.WriteLine(method.Name+" : "+method.ReturnType.Name);
+            }
+
+            Console.WriteLine("\n ------------------ Constructor in Customers ------------------");
+            ConstructorInfo[] constructors = T.GetConstructors();
+            foreach (ConstructorInfo constructor in constructors)
+            {
+                // name does not give more info
+                // Console.WriteLine(constructor.Name);
+                Console.WriteLine(constructor.ToString());
+            }
+        }
+    }
+
+    class Customer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public Customer(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+        public Customer()
+        {
+            Id = -1;
+            Name = string.Empty;
+        }
+
+        public void PrintId()
+        {
+            Console.WriteLine("ID = {0}",this.Id);
+        }
+
+        public void PrintName()
+        {
+            Console.WriteLine("Name = {0}", this.Name);
+        }
+    }
+}
